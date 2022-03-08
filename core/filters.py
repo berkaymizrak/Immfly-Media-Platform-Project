70628e789e24a816c83e26b88fcc8defea83524e
Berkay MIZRAK <berkay.mizrak@dorakholding.com.tr>
2022-03-08 14:12:57 +0300

    person endpoint added

--- core/filters.py
+++ core/filters.py
@@ -1,7 +1,22 @@
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
+from core import models
 
 
-class CustomFilterBackend(DjangoFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        qs = super().filter_queryset(request, queryset, view)
-        return qs
+class PersonFilter(filters.FilterSet):
+    first_name_autocomplete = filters.CharFilter(
+        field_name="first_name",
+        lookup_expr="icontains",
+    )
+    last_name_autocomplete = filters.CharFilter(
+        field_name="last_name",
+        lookup_expr="icontains",
+    )
+
+    class Meta:
+        model = models.Person
+        fields = (
+            "first_name",
+            "first_name_autocomplete",
+            "last_name",
+            "last_name_autocomplete",
+        )
